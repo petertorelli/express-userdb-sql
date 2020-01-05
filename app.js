73bad76985ab227c3dd42612011cc33d005b3940
petertorelli <peter.j.torelli@gmail.com>
2020-01-05 13:21:19 -0800

    Turned on a few things that were commented out.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const pingerRouter = require('./routers/pinger')
 
 const app = express()
 
-//app.use(helmet())
+app.use(helmet())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }));
@@ -25,11 +25,9 @@ var expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
 app.use(cookieSession({
   keys: [process.env.KEY1, process.env.KEY2],
   cookie: {
-  	/*
     secure: true,
     httpOnly: true,
     domain: 'example.com',
-    */
     expires: expiryDate
   }
 }))
@@ -48,33 +46,10 @@ app.use('/liddycam', liddyRouter)
 app.use('/pinger', pingerRouter)
 app.use('/', indexRouter)
 
-
-
 module.exports = app
 
 /*
 const createError = require('http-errors')
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
-
-const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
-
-const app = express()
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -91,5 +66,4 @@ app.use(function(err, req, res, next) {
 	res.status(err.status || 500)
 	res.render('error')
 })
-
 */
